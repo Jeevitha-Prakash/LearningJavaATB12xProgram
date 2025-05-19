a2f0f462a580c48640eec1fb5c37c1e8323a6ada
Jeevitha P <jeevitha.prakash2014@gmail.com>
2025-05-19 20:03:25 +0530

    17-05-2025-1

---
+++ src/Operators/Lab036_Operators_Assignment.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Lab036_Operators_Assignment {
+    public static void main(String[] args) {
+        int age =90;
+        //int -> datatype
+        //age -> Variable name(Identifier)
+        //= -> assignment operator
+        //90 -> Variable value(Literal)
+    }
+}
---
+++ src/Operators/Lab043_interview_Concat_Plus.java
@@ -0,0 +1,26 @@
+package Operators;
+
+public class Lab043_interview_Concat_Plus {
+    public static void main(String[] args) {
+
+        String first_name = "Jeevitha";
+        String last_name = "Prakash";
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println(first_name + last_name + a + b); // JeevithaPrakash1010
+        System.out.println(a + b + first_name + last_name);
+
+        System.out.println(first_name + last_name + (a + b));
+
+        // BODMAS - Bracket of Div, mul, add, sub
+
+
+
+
+
+
+    }
+}
+
